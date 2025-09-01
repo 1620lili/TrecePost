d30ecd105e42834d6207078c9959008d6fd3701f
liliana alejandra valencia velasquez <victor escobar>
2025-08-31 23:27:32 -0500

    avance en mi proyecto

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismasService } from "../prisma/prisma.service";
+import { CreateUserDto } from "./dto/create_user.dto"
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismasService) {
+
+    async create(data: CreateUserDto){
+      return this.prisma.user.create({
+        data: {
+          nombre: data.nombre,
+          email: data.email
+          password: data.password,
+          rolId: data.rolId
+        }
+      })
+    }
+  }
+}
